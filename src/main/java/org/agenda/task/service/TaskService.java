bd589afbf3a51f5737890b0ab8c44a496d9c5cfc
BORGES <jessborgesb@gmail.com>
2026-05-07 20:15:18 +0200

    feat: add TaskService interface

--- src/main/java/org/agenda/task/service/TaskService.java
+++ src/main/java/org/agenda/task/service/TaskService.java
@@ -1,10 +1,25 @@
 package org.agenda.task.service;
 
-import org.agenda.task.model.Task;
+import org.agenda.task.dto.TaskRequest;
+import org.agenda.task.dto.TaskResponse;
 
 import java.util.List;
 
 public interface TaskService {
-    void createTask(Task task);
-    List<Task> getAllTasks();
+
+    TaskResponse create(TaskRequest request);
+
+    TaskResponse getById(Long id);
+
+    List<TaskResponse> getAll();
+
+    List<TaskResponse> getPendingTasks();
+
+    List<TaskResponse> getCompletedTasks();
+
+    TaskResponse update(Long id, TaskRequest request);
+
+    TaskResponse markAsDone(Long id);
+
+    boolean delete(Long id);
 }

861a8c490b6fbafe1bb02a6eb4f3e21dc388ed1e
adriElias <34091624+adriElias@users.noreply.github.com>
2026-05-07 10:53:06 +0200

    chore:update init.sql file

--- sql/init.sql
+++ sql/init.sql
@@ -10,26 +10,26 @@ DROP TABLE IF EXISTS event;
 
 CREATE TABLE event (
     id           INT UNSIGNED    AUTO_INCREMENT PRIMARY KEY,
-    title        VARCHAR(500)    NOT NULL,
+    title        VARCHAR(100)    NOT NULL,
     body         TEXT,
-    event_date   TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    event_type   ENUM('BIRTHDATE','APPOINTMENT','REMINDER','OTHER')
+    date  		 TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    type   		 ENUM('BIRTHDATE','APPOINTMENT','REMINDER','OTHER')
                                  NOT NULL DEFAULT 'OTHER',
-    repetition   VARCHAR(250),
+    schedule     ENUM('@yearly','@monthly','@weekly','@daily', '@hourly'),
     created_at   TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at   TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP
                                  ON UPDATE CURRENT_TIMESTAMP,
-    INDEX idx_event_date (event_date),
-    INDEX idx_event_type (event_type)
+    INDEX idx_event_date (date),
+    INDEX idx_event_type (type)
 ) ENGINE = InnoDB;
 
 CREATE TABLE task (
     id              INT UNSIGNED    AUTO_INCREMENT PRIMARY KEY,
-    title           VARCHAR(500)    NOT NULL,
-    body            TEXT            NOT NULL,
-    status_task     ENUM('PENDING','IN PROGRESS','DONE')
+    title           VARCHAR(100)    NOT NULL,
+    body            TEXT,
+    status     		ENUM('PENDING','IN PROGRESS','DONE')
                                     NOT NULL DEFAULT 'PENDING',
-    priority_type   ENUM('HIGH','MEDIUM','LOW'),
+    priority   		ENUM('HIGH','MEDIUM','LOW'),
     expiration_date TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -38,13 +38,13 @@ CREATE TABLE task (
     CONSTRAINT fk_task_event
         FOREIGN KEY (event_id) REFERENCES event (id)
         ON DELETE SET NULL,
-    INDEX idx_status       (status_task),
-    INDEX idx_priority_type (priority_type)
+    INDEX idx_task_status       (status),
+    INDEX idx_task_priority 	(priority)
 ) ENGINE = InnoDB;
 
 CREATE TABLE note (
     id         INT UNSIGNED    AUTO_INCREMENT PRIMARY KEY,
-    title      VARCHAR(500)    NOT NULL DEFAULT '',
+    title      VARCHAR(100)    NOT NULL,
     body       TEXT,
     created_at TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP

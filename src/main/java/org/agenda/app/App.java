fbc58196368816cb7ed82c4711977ebb636a25b7
adriElias <34091624+adriElias@users.noreply.github.com>
2026-05-08 17:54:53 +0200

    chore: update init.sql file

--- src/main/java/org/agenda/app/App.java
+++ src/main/java/org/agenda/app/App.java
@@ -2,6 +2,11 @@ package org.agenda.app;
 
 public class App {
 
+    public static void main(String[] args) {
+        System.out.println("Agenda Application Starting...");
+        run();
+    }
+
     public static void run() {
         System.out.println("Agenda Application Starting...");
     }

dce90d65891ad6dfab71918774b36f3f961d4c0f
BORGES <jessborgesb@gmail.com>
2026-05-13 11:33:45 +0200

    feat: integrate Task domain into App entry point

--- src/main/java/org/agenda/app/App.java
+++ src/main/java/org/agenda/app/App.java
@@ -24,7 +24,7 @@ public class App {
             // NOTE DOMAIN
 
             // EVENT DOMAIN
-
+            git checkout feature/task-domain
 
             showMainMenu(scanner, taskController);
 

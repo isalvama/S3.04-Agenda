8f1c52ad98ad6af8145ef2e8bd2ccae4d3d8a694
adriElias <34091624+adriElias@users.noreply.github.com>
2026-05-15 12:38:28 +0200

    feat: add NoteRepository interface

--- src/main/java/org/agenda/note/repository/NoteRepository.java
+++ src/main/java/org/agenda/note/repository/NoteRepository.java
@@ -7,9 +7,14 @@ import java.util.Optional;
 
 public interface NoteRepository {
     Note save(Note note);
+
     Optional<Note> findById(Long id);
+
     List<Note> findAllByTaskId(Long taskId);
+
     List<Note> findAll();
+
     boolean deleteById(Long id);
+
     boolean existsById(Long id);
 }

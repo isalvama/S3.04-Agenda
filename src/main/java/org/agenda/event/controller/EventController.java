10b3b395525f8a38f3c070a97fb9875352a36be7
isalvama <inesalvans@gmail.com>
2026-05-18 21:35:47 +0200

    fix: adapt EventController to project's controllers structure and test it with EventControllerTest

--- src/main/java/org/agenda/event/controller/EventController.java
+++ src/main/java/org/agenda/event/controller/EventController.java
@@ -7,7 +7,6 @@ import org.agenda.event.model.EventSchedule;
 import org.agenda.event.model.EventType;
 import org.agenda.event.repository.EventNotFoundException;
 import org.agenda.event.repository.EventNotSavedException;
-import org.agenda.event.repository.EventRepository;
 import org.agenda.event.service.EventService;
 import org.agenda.shared.console.ConsoleReader;
 import org.agenda.shared.domain.exception.DomainException;
@@ -15,7 +14,9 @@ import org.agenda.shared.exception.DataAccessException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import static org.agenda.shared.console.ConsoleReader.readLong;
@@ -24,54 +25,63 @@ import static org.agenda.shared.console.ConsoleReader.readLong;
 public class EventController {
 
     private final EventService eventService;
-    private final EventRepository repository;
     private final Scanner scanner;
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    private final EventFormatter eventFormatter = new EventFormatter();
+    private final Map<String, Runnable> menuActions;
 
 
-    public EventController(EventService eventService, EventRepository repository, Scanner scanner) {
+    public EventController(EventService eventService, Scanner scanner) {
         this.eventService = eventService;
         this.scanner = scanner;
-        this.repository = repository;
+        this.menuActions = new HashMap<>();
+        initializeMenu();
     }
 
-    public void showMenu() {
+    private void initializeMenu() {
+        menuActions.put("1", this::createEvent);
+        menuActions.put("2", this::listAllEvents);
+        menuActions.put("3", this::listUpcomingEvents);
+        menuActions.put("4", this::updateEvent);
+        menuActions.put("5", this::deleteEvent);
+        menuActions.put("6", this::findEventById);
+        menuActions.put("0", () -> System.out.println("Returning to main menu..."));
+    }
+
+    public void showMenu(){
         boolean running = true;
-
         while (running) {
-            System.out.println("\n--- EVENT MANAGEMENT ---");
-            System.out.println("1. Create an event");
-            System.out.println("2. List all events");
-            System.out.println("3. List upcoming events");
-            System.out.println("4. Update an event");
-            System.out.println("5. Delete an event");
-            System.out.println("6. Find an event by ID");
-            System.out.println("0. Back to main menu");
-            System.out.println("Select an option:");
+            printMenu();
+            String option = ConsoleReader.readString("Choose an option:", 1);
 
-            String choice = scanner.nextLine().trim();
-
-            switch (choice) {
-                case "1" -> createEvent();
-                case "2" -> listAllEvents();
-                case "3" -> listUpcomingEvents();
-                case "4" -> updateEvent();
-                case "5" -> deleteEvent();
-                case "6" -> findEventById();
-                case "0" -> {
-                    System.out.println("Returning to main menu...");
-                    running = false;
-                }
-                default -> System.out.println("Invalid option. Please try again.");
+            if ("0".equals(option)) {
+                System.out.println("Returning to main menu...");
+                running = false;
+            } else {
+                menuActions.getOrDefault(option,
+                        () -> System.out.println("Invalid option. Please try again.")).run();
             }
         }
+    }
+
+
+    public void printMenu() {
+        System.out.println("\n--- EVENT MANAGEMENT ---");
+        System.out.println("1. Create an event");
+        System.out.println("2. List all events");
+        System.out.println("3. List upcoming events");
+        System.out.println("4. Update an event");
+        System.out.println("5. Delete an event");
+        System.out.println("6. Find an event by ID");
+        System.out.println("0. Back to main menu");
+        System.out.println("Select an option:");
     }
 
     private void createEvent() {
         CreateEventRequest request = collectData("");
         try {
             EventResponse eventResponse = eventService.create(request);
-            System.out.println("Event created with ID " + eventResponse.id() + ":\n" + formatEventResponse(eventResponse));
+            System.out.println("Event created with ID " + eventResponse.id() + ":\n" + eventFormatter.formatEventResponse(eventResponse));
         } catch (DomainException e) {
             System.out.println("Domain Error: " + e.getMessage());
         } catch (EventNotSavedException e) {
@@ -93,7 +103,7 @@ public class EventController {
                 return;
             }
 
-            System.out.println("List of found Events:\n" + formatEventResponses(eventResponses));
+            System.out.println("List of found Events:\n" + eventFormatter.formatEventResponses(eventResponses));
 
         } catch (DomainException e) {
             System.out.println("Domain Error: " + e.getMessage());
@@ -102,15 +112,30 @@ public class EventController {
         }
     }
 
+    public void showTodayAndTomorrowEvents(){
+        try {
+            List<EventResponse> eventResponses = eventService.getUpcomingEvents(1);
+            if (eventResponses.isEmpty()) {
+                System.out.println("There are no events for today or tomorrow");
+                return;
+            }
+            System.out.println("List of Upcoming Events for today and tomorrow:\n" + eventFormatter.formatEventResponses(eventResponses));
+        } catch (DomainException e) {
+            System.out.println("Domain Error: " + e.getMessage());
+        } catch (DataAccessException e) {
+            System.out.println("Error in DataBase: " + e.getMessage());
+        }
+    }
+
     private void listUpcomingEvents() {
-        int days = ConsoleReader.readInt("\nEnter the number of days to display the next events (e.g., 7)."); //TODO revisar si se puede poner numero negativo y consecuencias
+        int days = ConsoleReader.readInt("\nEnter the number of days to display the next events (e.g., 5)");
         try {
             List<EventResponse> eventResponses = eventService.getUpcomingEvents(days);
             if (eventResponses.isEmpty()) {
                 System.out.printf("There are no events in the next %s days", days);
                 return;
             }
-            System.out.println("List of Upcoming Events (in the next " + days + " days):\n" + formatEventResponses(eventResponses));
+            System.out.println("List of Upcoming Events (in the next " + days + " days):\n" + eventFormatter.formatEventResponses(eventResponses));
         } catch (DomainException e) {
             System.out.println("Domain Error: " + e.getMessage());
         } catch (DataAccessException e) {
@@ -125,11 +150,11 @@ public class EventController {
 
         try {
             EventResponse eventResponse = eventService.update(request);
-            System.out.printf("Task # %s updated successfully: %s", eventResponse.id(), formatEventResponse(eventResponse));
+            System.out.printf("Task # %s updated successfully: %s", eventResponse.id(), eventFormatter.formatEventResponse(eventResponse));
         } catch (DomainException e) {
             System.out.println("Domain Error: " + e.getMessage());
         } catch (DataAccessException e) {
-            System.out.println("Error in DataBase: " + e.getMessage());
+            System.out.println("Error in DataBase process: " + e.getMessage());
         } catch (EventNotFoundException e) {
             System.out.println("Error: " + e.getMessage());
         }
@@ -139,7 +164,7 @@ public class EventController {
         Long id = ConsoleReader.readLong("Enter an Event ID to delete: ");
         try {
             EventResponse eventResponse = eventService.delete(id);
-            System.out.printf("Event #%s deleted successfully: %s", eventResponse.id(), formatEventResponse(eventResponse));
+            System.out.printf("Event #%s deleted successfully: %s", eventResponse.id(), eventFormatter.formatEventResponse(eventResponse));
         } catch (DomainException e) {
             System.out.println("Domain Error: " + e.getMessage());
         } catch (DataAccessException e) {
@@ -153,7 +178,7 @@ public class EventController {
         Long id = ConsoleReader.readLong("Enter the Event ID to find: ");
         try {
             EventResponse response = eventService.getById(id);
-            System.out.printf("Event #%s found successfully : %s", response.id(), formatEventResponse(response));
+            System.out.printf("Event #%s found successfully : %s", response.id(), eventFormatter.formatEventResponse(response));
         } catch (DomainException e) {
             System.out.println("Domain Error: " + e.getMessage());
         } catch (DataAccessException e) {
@@ -178,26 +203,5 @@ public class EventController {
         String repetition = ConsoleReader.readEnumName(EventSchedule.class, String.format("%sSchedule a repetition for the event (YEARLY / MONTHLY / WEEKLY / DAILY / HOURLY or leave empty for none):", wordToAddToQueries != null ? wordToAddToQueries : ""));
 
         return new CreateEventRequest(title, body, date, type, repetition);
-    }
-
-    private String formatEventResponse(EventResponse eventResponse) {
-        return String.format("Id: %s | Title: %s | Description: %s | Date: %s | Type: %s | Schedule: %s. %s\n",
-                eventResponse.id(),
-                eventResponse.title(),
-                eventResponse.description(),
-                eventResponse.date(),
-                eventResponse.type(),
-                eventResponse.schedule(),
-                eventResponse.warnings().toString()
-        );
-    }
-
-    private String formatEventResponses(List<EventResponse> eventResponses) {
-        StringBuilder response = new StringBuilder();
-        for (EventResponse eventResponse : eventResponses) {
-            response.append(formatEventResponse(eventResponse)
-            );
-        }
-        return response.toString();
     }
 }

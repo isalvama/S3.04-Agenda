260bd9dec305edef2a6b8273d21ad271e6dd84eb
isalvama <inesalvans@gmail.com>
2026-05-18 21:32:49 +0200

    feat: add negative validation to readInt() method of ConsoleReader class

--- src/main/java/org/agenda/shared/console/ConsoleReader.java
+++ src/main/java/org/agenda/shared/console/ConsoleReader.java
@@ -31,8 +31,10 @@ public class ConsoleReader {
         while (true){
             System.out.println(message);
             try {
-                return Integer.parseInt(scanner.nextLine().trim());
-            } catch (NumberFormatException e){
+                int number = Integer.parseInt(scanner.nextLine().trim());
+                if (number < 0) throw new RuntimeException("Number should not be negative");
+                return number;
+            } catch (RuntimeException e){
                 System.out.printf("Invalid Input: %s. Please enter a valid number.", e.getMessage());
             }
         }
